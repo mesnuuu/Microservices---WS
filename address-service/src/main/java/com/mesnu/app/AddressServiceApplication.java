8813b232313675bce77b25c44bd7094e018982d7
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2022-10-25 11:21:37 +0500

    Eureka Clients - Configured
    1. Add Dependency
    2. Enable Eureka
    3. Set properties

--- address-service/src/main/java/com/mesnu/app/AddressServiceApplication.java
+++ address-service/src/main/java/com/mesnu/app/AddressServiceApplication.java
@@ -3,6 +3,7 @@ package com.mesnu.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan({"com.mesnu.controller", "com.mesnu.service"})
 @EntityScan("com.mesnu.entity")
 @EnableJpaRepositories("com.mesnu.repository")
+@EnableEurekaClient
 public class AddressServiceApplication {
 
 	public static void main(String[] args) {
