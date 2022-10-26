3e44fe2680221c9077342fef1639c0219f8d6ba2
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2022-10-26 06:24:13 +0500

    Client Side Load Balancing with Spring Cloud Load Balancer
    1.  Add Dependency
    2.  create Load Balancer Client
    3. configure client
    
    4. Run multiple instances by changing port and test

---
+++ student-service/src/main/java/com/mesnu/feignclients/AdrSerLoadBalConfig.java
@@ -0,0 +1,18 @@
+package com.mesnu.feignclients;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+
+import feign.Feign;
+
+@LoadBalancerClient(value = "address-service")
+public class AdrSerLoadBalConfig {
+    
+    @Bean
+    @LoadBalanced
+    public Feign.Builder feignBuilder () {
+        return Feign.builder();
+    }
+
+}
