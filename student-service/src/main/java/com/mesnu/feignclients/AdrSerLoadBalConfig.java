4f76af3489d5d795cd1b985b4a99ba71432e4f14
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2022-10-27 09:16:39 +0500

    Load Balancing with API Gateway
    When we use api-gateway in Feign client load balancing will automatically be handles by api-gateway
    
    We don't need to worry about it now.

--- student-service/src/main/java/com/mesnu/feignclients/AdrSerLoadBalConfig.java
+++ student-service/src/main/java/com/mesnu/feignclients/AdrSerLoadBalConfig.java
@@ -1,18 +1,18 @@
-package com.mesnu.feignclients;
+// package com.mesnu.feignclients;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+// import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+// import org.springframework.context.annotation.Bean;
 
-import feign.Feign;
+// import feign.Feign;
 
-@LoadBalancerClient(value = "address-service")
-public class AdrSerLoadBalConfig {
+// @LoadBalancerClient(value = "address-service")
+// public class AdrSerLoadBalConfig {
     
-    @Bean
-    @LoadBalanced
-    public Feign.Builder feignBuilder () {
-        return Feign.builder();
-    }
+//     @Bean
+//     @LoadBalanced
+//     public Feign.Builder feignBuilder () {
+//         return Feign.builder();
+//     }
 
-}
+// }
