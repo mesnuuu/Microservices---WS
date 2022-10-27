5193c36ee89a2e26827b2bd1ebd504e090d31d02
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2022-10-27 09:03:37 +0500

    Feign Client with API Gateway - for internal calls
    example - request and response
    m1  -> gateway
    gateway -> m2
    m2 -> gateway
    gateway -> m1

--- student-service/src/main/java/com/mesnu/feignclients/AddressFeignClient.java
+++ student-service/src/main/java/com/mesnu/feignclients/AddressFeignClient.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.mesnu.response.AddressResponse;
 
-@FeignClient(value = "address-service", path = "/api/address")
+// @FeignClient(value = "address-service", path = "/api/address")
+
+@FeignClient(value = "api-gateway")
 
 public interface AddressFeignClient {
 
-    @GetMapping("/getById/{id}")
+    @GetMapping("address-service/api/address/getById/{id}")
     public AddressResponse getById(@PathVariable long id);
 
 }
